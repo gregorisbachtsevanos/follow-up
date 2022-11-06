8d26cfba919f9e17a91ce678f90d5b78862426de
Gregoris Bachtsevanos <gregorbachts@gmail.com>
2022-11-06 19:34:27 +0200

    update profile error

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -70,7 +70,7 @@ export const useFirestore = (collection) => {
 	const updateDocument = async (id, updates) => {
 		dispatch({ type: 'IS_PENDING' });
 		const newInfo = updates
-		return console.log(id, {...updates})
+		// return console.log(id, {...updates})
 		const updatedDocument = await ref.doc(id).update(updates);
 		try {
 			dispatch({ type: 'UPDATE_DOCUMENT', payload: updatedDocument });
